5894cb7a83384235e70191636a760b234dcf1ced
Chris Anderton <chrisanderton@users.noreply.github.com>
2016-11-04 18:13:31 +0000

    if exists

--- .profile.d/pio-render-configs.sh
+++ .profile.d/pio-render-configs.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-erb /app/pio-engine/PredictionIO-dist/conf/core-site.xml.erb > /app/pio-engine/PredictionIO-dist/conf/core-site.xml
+if [ -f "/app/pio-engine/PredictionIO-dist/conf/core-site.xml.erb" ]
+then
+  erb /app/pio-engine/PredictionIO-dist/conf/core-site.xml.erb > /app/pio-engine/PredictionIO-dist/conf/core-site.xml
+fi
